5e4b86be116cd2b3d3b4d61a0a59afc883887cf3
Rashmi G R <rashmigr01@gmail.com>
2025-08-29 00:04:41 +0530

    fix: add animation to link, fix text size

--- bare-portfolio/src/pages/home/components/Contact.tsx
+++ bare-portfolio/src/pages/home/components/Contact.tsx
@@ -4,34 +4,55 @@ import { Link } from "react-router";
 
 const Contact = () => {
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-4 text-sm">
       <div className="flex items-center justify-between gap-2">
         <Mail className="size-4" />
-        <Link to="mailto:name@email.com" className="hover:underline">name@email.com</Link>
+        <Link to="mailto:name@email.com" className="relative group">
+          name@email.com
+          <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-current transition-all duration-300 group-hover:w-full"/>
+        </Link>
       </div>
       <div className="flex items-center justify-between gap-2">
         <Github className="size-4" />
-        <Link to="https://github.com/username" className="hover:underline">username</Link>
+        <Link to="https://github.com/username" className="relative group">
+          username
+          <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-current transition-all duration-200 group-hover:w-full"/>
+        </Link>
       </div>
       <div className="flex items-center justify-between gap-2">
         <Linkedin className="size-4" />
-        <Link to="https://linkedin.com/in/username" className="hover:underline">username</Link>
+        <Link to="https://linkedin.com/in/username" className="relative group">
+          username
+          <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-current transition-all duration-200 group-hover:w-full"/>
+        </Link>
       </div>
       <div className="flex items-center justify-between gap-2">
         <X className="size-4" />
-        <Link to="https://x.com/username" className="hover:underline">username</Link>
+        <Link to="https://x.com/username" className="relative group">
+          username
+          <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-current transition-all duration-200 group-hover:w-full"/>
+        </Link>
       </div>
       <div className="flex items-center justify-between gap-2">
         <Bluesky className="size-4" />
-        <Link to="https://bsky.app/profile/username" className="hover:underline">username</Link>
+        <Link to="https://bsky.app/profile/username" className="relative group">
+          username
+          <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-current transition-all duration-200 group-hover:w-full"/>
+        </Link>
       </div>
       <div className="flex items-center justify-between gap-2">
         <Instagram className="size-4" />
-        <Link to="https://instagram.com/username" className="hover:underline">username</Link>
+        <Link to="https://instagram.com/username" className="relative group">
+          username
+          <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-current transition-all duration-200 group-hover:w-full"/>
+        </Link>
       </div>
       <div className="flex items-center justify-between gap-2">
         <Facebook className="size-4" />
-        <Link to="https://facebook.com/username" className="hover:underline">username</Link>
+        <Link to="https://facebook.com/username" className="relative group">
+          username
+          <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-current transition-all duration-200 group-hover:w-full"/>
+        </Link>
       </div>
     </div>
   );
