67ce9a7ba812d94b9886a160cbf39cac480f122b
Rashmi G R <rashmigr01@gmail.com>
2025-08-31 00:44:39 +0530

    fix: contact links

--- bare-portfolio/src/pages/home/sections/Contact.tsx
+++ bare-portfolio/src/pages/home/sections/Contact.tsx
@@ -30,42 +30,42 @@ const Contact = () => {
       </div>
       <div className="flex items-center justify-between gap-4">
         <Github className="size-4" />
-        <Link to={GITHUB} className="relative group truncate">
+        <Link to={`https://${GITHUB}`} className="relative group truncate">
           {GITHUB}
           <span className="absolute bottom-0 left-0 w-0 h-[1px] bg-current transition-all duration-200 group-hover:w-full" />
         </Link>
       </div>
       <div className="flex items-center justify-between gap-4">
         <Linkedin className="size-4" />
-        <Link to={LINKEDIN} className="relative group truncate">
+        <Link to={`https://${LINKEDIN}`} className="relative group truncate">
           {LINKEDIN}
           <span className="absolute bottom-0 left-0 w-0 h-[1px] bg-current transition-all duration-200 group-hover:w-full" />
         </Link>
       </div>
       <div className="flex items-center justify-between gap-4">
         <X className="size-4" />
-        <Link to={X_INFO} className="relative group truncate">
+        <Link to={`https://${X_INFO}`} className="relative group truncate">
           {X_INFO}
           <span className="absolute bottom-0 left-0 w-0 h-[1px] bg-current transition-all duration-200 group-hover:w-full" />
         </Link>
       </div>
       <div className="flex items-center justify-between gap-4">
         <Bluesky className="size-4" />
-        <Link to={BLUESKY} className="relative group truncate">
+        <Link to={`https://${BLUESKY}`} className="relative group truncate">
           {BLUESKY}
           <span className="absolute bottom-0 left-0 w-0 h-[1px] bg-current transition-all duration-200 group-hover:w-full" />
         </Link>
       </div>
       <div className="flex items-center justify-between gap-4">
         <Instagram className="size-4" />
-        <Link to={INSTAGRAM} className="relative group truncate">
+        <Link to={`https://${INSTAGRAM}`} className="relative group truncate">
           {INSTAGRAM}
           <span className="absolute bottom-0 left-0 w-0 h-[1px] bg-current transition-all duration-200 group-hover:w-full" />
         </Link>
       </div>
       <div className="flex items-center justify-between gap-4">
         <Facebook className="size-4" />
-        <Link to={FACEBOOK} className="relative group truncate">
+        <Link to={`https://${FACEBOOK}`} className="relative group truncate">
           {FACEBOOK}
           <span className="absolute bottom-0 left-0 w-0 h-[1px] bg-current transition-all duration-200 group-hover:w-full" />
         </Link>
